2f9c55ca7f5646b9877b82afcfd14a0de51cc660
eloudsa <said.eloudrhiri@lampiris.be>
2019-11-02 19:58:06 +0100

    tests compliant with Mocha

--- test/GreeetingsTest.js
+++ test/GreeetingsTest.js
@@ -1,15 +1,41 @@
-const greetingsContract = artifacts.require('Greetings');
+const Greetings = artifacts.require('Greetings');
 
 // test suite
-contract('Greetings', async accounts => {
+describe('Greetings', () => {
+  let accounts;
   let contractInstance;
   const defaultMessage = 'Hello from Devoxx Morocco 2019!';
   const newMessage = 'Hello from Agadir!';
 
-  before('setup contract for each test', async () => {
-    contractInstance = await greetingsContract.deployed();
+  before(async () => {
+    accounts = await web3.eth.getAccounts();
+    contractInstance = await Greetings.new();
+    console.log(contractInstance);
   });
 
+  it('should let us get the initial message', async () => {
+    // check that we have properly deployed our contract
+    assert.equal(
+      await contractInstance.getGreetings(),
+      defaultMessage,
+      'The default greetings message shoud be ' + defaultMessage
+    );
+  });
+
+  it('should let us change the initial message', async () => {
+    // change the greetings message
+    await contractInstance.setGreetings(newMessage, {
+      from: accounts[1]
+    });
+
+    // check that we have properly deployed our contract
+    assert.equal(
+      await contractInstance.getGreetings(),
+      newMessage,
+      'The new greetings message shoud be ' + newMessage
+    );
+  });
+  /*
   it('should let us get the initial message', async () => {
     // retrieve the current greetings message
     const greetings = await contractInstance.getGreetings();
@@ -38,4 +64,5 @@ contract('Greetings', async accounts => {
       'The new greetings message shoud be ' + newMessage
     );
   });
+  */
 });
