b6cee260e2a713116fa70165e94f7812fb3332ad
eloudsa <said.eloudrhiri@lampiris.be>
2019-11-04 16:21:21 +0100

    inheritance and modifiers

--- contracts/Greetings.sol
+++ contracts/Greetings.sol
@@ -1,45 +1,46 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract Greetings {
+import "./Ownable.sol";
+
+contract Greetings is Ownable {
     string message;
     uint256 serviceFee;
-    address payable public owner;
     bool public enable;
 
     // Events
     event GreetingsChangedEvent (address indexed _account, string _greetings);
     event NewServiceFeeEvent (uint256 _serviceFee);
 
+    // Modifiers
+    modifier onlyEnable() {
+        // only if the contract is enabled
+        require(enable == true , "The contract is not enabled!");
+        _;
+    }
 
     constructor(uint256 _serviceFee, bool _enable) public {
         message = "Hello from Devoxx Morocco 2019!";
         serviceFee = _serviceFee;
-        owner = msg.sender;
         enable = _enable;
     }
     
     // kill the smart contract 
-    function kill() public {
-        require(msg.sender == owner, "Your are not the owner of this contract!");      
+    function kill() public onlyOwner {
         selfdestruct(msg.sender);
     }
 
     // Enable the smart contract 
-    function enableContract() public {
-        require(msg.sender == owner, "Your are not the owner of this contract!");      
+    function enableContract() public onlyOwner {
         enable = true;
     }
 
     // kill the smart contract 
-    function disableContract() public {
-        require(msg.sender == owner, "Your are not the owner of this contract!");      
+    function disableContract() public onlyOwner {
         enable = false;
     }
 
     // Setters
-    function setGreetings(string memory _message) payable public {
-        require(enable == true , "The contract is not enabled!");
-
+    function setGreetings(string memory _message) payable public onlyEnable {
         bytes memory newMessage = bytes(_message);
         require(newMessage.length > 0 , "The message should not be empty!");
 
@@ -50,19 +51,13 @@ contract Greetings {
         emit GreetingsChangedEvent(msg.sender, _message);
     }
 
-    function setServiceFee(uint256 _serviceFee) public {
-        require(enable == true , "The contract is not enabled!");
-        
-        require(msg.sender == owner , "Your are not the owner of this contract!");
-        
+    function setServiceFee(uint256 _serviceFee) public onlyOwner {        
         serviceFee = _serviceFee;
     
         emit NewServiceFeeEvent(_serviceFee);
     }
 
-    function transferEarning() public {
-        require(msg.sender == owner , "Your are not the owner of this contract!");
-        
+    function transferEarning() public onlyOwner {
         // transfer earning if any
         if (address(this).balance > 0) {
             owner.transfer(address(this).balance);
