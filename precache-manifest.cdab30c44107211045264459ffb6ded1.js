43407844297bf1d6ab26817c7e13f2a3d2cb12e1
Said Eloudrhiri <info@noratek.net>
2019-11-08 22:49:21 +0100

    Updates

---
+++ precache-manifest.cdab30c44107211045264459ffb6ded1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce591496feb49805049d8fb574c9a4af",
+    "url": "/devoxxma2019-greetings-truffle/client/index.html"
+  },
+  {
+    "revision": "a9b93bdfeba3a763d7ca",
+    "url": "/devoxxma2019-greetings-truffle/client/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "912eb97b8e4b0ce3619a",
+    "url": "/devoxxma2019-greetings-truffle/client/static/css/main.df3e89bc.chunk.css"
+  },
+  {
+    "revision": "a9b93bdfeba3a763d7ca",
+    "url": "/devoxxma2019-greetings-truffle/client/static/js/2.79b50ba2.chunk.js"
+  },
+  {
+    "revision": "912eb97b8e4b0ce3619a",
+    "url": "/devoxxma2019-greetings-truffle/client/static/js/main.ab822918.chunk.js"
+  },
+  {
+    "revision": "1ab93fc86d17d7e836ea",
+    "url": "/devoxxma2019-greetings-truffle/client/static/js/runtime-main.2ef53e89.js"
+  }
+]);
