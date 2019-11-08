efd5ed4f1113b8e196a362a139f4b2b001b51bda
eloudsa <said.eloudrhiri@lampiris.be>
2019-11-08 12:18:38 +0100

    add controls

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import JazzIcon, {jsNumberForAddress} from "react-jazzicon";
 
 import "materialize-css/dist/css/materialize.min.css";
@@ -11,12 +11,23 @@ const App = ({drizzleContext}) => {
   const [account, setAccount] = useState(null);
   const [balance, setBalance] = useState(0);
 
+  const refFirstField = useRef(null);
+  const [greetings, setGreetings] = useState("");
+  const [newGreetings, setNewGreetings] = useState("");
+
   useEffect(() => {
-    // Initialize Materialize JS
     M.AutoInit();
 
     // eslint-disable-next-line
   }, []);
+
+  const onChange = e => {
+    setGreetings(e.target.value);
+  };
+
+  const onSubmit = () => {
+    setNewGreetings(greetings);
+  };
 
   useEffect(() => {
     if (initialized) {
@@ -26,6 +37,14 @@ const App = ({drizzleContext}) => {
 
         setAccount(currAccout);
         setBalance(drizzle.web3.utils.fromWei(currBalance, "ether"));
+
+        M.CharacterCounter.init(
+          document.querySelectorAll(".has-character-counter")
+        );
+
+        if (refFirstField !== null) {
+          refFirstField.current.focus();
+        }
       }
       fetchAccount();
     }
@@ -49,12 +68,48 @@ const App = ({drizzleContext}) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col s12 m6 offset-m3">
+        <div className="col m12">
           <div className="avatar">
             <JazzIcon diameter={40} seed={jsNumberForAddress(account)} />
             <p className="account">{account}</p>
             <p className="balance">{balance} ETH</p>
           </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col m12">
+          <p>The current greetings is:</p>
+          <p>{newGreetings}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col m8 input-field">
+          <textarea
+            className="materialize-textarea has-character-counter"
+            ref={refFirstField}
+            type="text"
+            name="title"
+            value={greetings}
+            data-length={80}
+            onChange={onChange}
+            style={{height: "4rem"}}
+          />
+
+          <label htmlFor="title" className="active">
+            Enter a new Greetings
+          </label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col m8 input-field">
+          <a
+            href="#!"
+            className="col s12 m2 waves-effect waves-light btn-small right primary-content blue"
+            onClick={() => onSubmit()}
+            style={{margin: "5px"}}
+          >
+            Save
+          </a>
         </div>
       </div>
     </div>
