6d3c5b19e4a9ebd2d06eeeb11bb233c9e89061d4
eloudsa <said.eloudrhiri@lampiris.be>
2019-11-09 14:01:16 +0100

    Updates

---
+++ precache-manifest.92a187e6e4ef3f298bdb0aca29686588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dab2b0a9cc8bb4982b823af839b316",
+    "url": "/devoxxma2019-greetings-truffle/index.html"
+  },
+  {
+    "revision": "c2c98d87d7e0746cc722",
+    "url": "/devoxxma2019-greetings-truffle/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "fc7299a4a7aa6c1579a5",
+    "url": "/devoxxma2019-greetings-truffle/static/css/main.df3e89bc.chunk.css"
+  },
+  {
+    "revision": "c2c98d87d7e0746cc722",
+    "url": "/devoxxma2019-greetings-truffle/static/js/2.4d96ed46.chunk.js"
+  },
+  {
+    "revision": "fc7299a4a7aa6c1579a5",
+    "url": "/devoxxma2019-greetings-truffle/static/js/main.84e0bf2a.chunk.js"
+  },
+  {
+    "revision": "e4f78783362d0722a7f2",
+    "url": "/devoxxma2019-greetings-truffle/static/js/runtime-main.f6235dc9.js"
+  }
+]);
